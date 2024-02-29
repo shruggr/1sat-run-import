64bc910d872192dd68c7feeaae862e3bbf2ce748
David Case <david@fyxgaming.com>
2024-02-29 13:28:19 -0500

    log redis string

--- cache.js
+++ cache.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const Redis = require('ioredis');
-const redis = new Redis(`${process.env.REDIS_URL}`);
+const { REDIS_URL } = process.env;
+console.log("REDIS:", REDIS_URL);
+const redis = new Redis(`${REDIS_URL}`);
 
 class Cache {
     async get(key) {
